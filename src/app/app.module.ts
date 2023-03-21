9db72782cbefa7df436f2380315847b30327bf19
Carole TOULORGE <carole.toulorge@hotmail.fr>
2023-03-21 12:31:36 +0100

    creation components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { MainComponent } from './components/main/main.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    NavComponent,
+    LogoComponent,
+    FooterComponent,
+    ListComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [ BrowserModule ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
