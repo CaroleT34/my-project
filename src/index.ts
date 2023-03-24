f39448a95f7168c9757b933060bb52b95f9bd7f6
Carole TOULORGE <carole.toulorge@hotmail.fr>
2023-03-24 14:19:00 +0100

    code pour générer carré

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,3 @@ import { App } from './app/app';
 import './index.scss';
 
 App.run();
-
-interface Brick {
-    color: string;
-    addNewBrick: () => void;
-  }
-  
-  const brickBlue: Brick = {
-    color: 'blue',
-    addNewBrick() {
-      console.log(this.color);
-    }
-  };
-
-  const newBrick: Brick = {
-    color: "red",
-    addNewBrick: () => {
-      console.log(newBrick, "newBrick")
-    }
-  };
-  
-  function addNewBrick() {
-    const brickDiv = document.getElementById("brick");
-    const newSquare = document.createElement("div");
-    newSquare.style.backgroundColor = "red";
-    newSquare.style.width = "50px";
-    newSquare.style.height = "50px";
-    newSquare.style.margin = "5px";
-    brickDiv.appendChild(newSquare);
-  }
-
-  
-  const addNewBrickButton = document.getElementById("addNewBrickButton");
-  addNewBrickButton.addEventListener("click", addNewBrick);
