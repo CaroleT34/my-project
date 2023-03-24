1206315fac6ce81c446a8ff4a1b8a72dc3cde998
Carole TOULORGE <carole.toulorge@hotmail.fr>
2023-03-24 11:36:00 +0100

    ts relié à html + génére des carrés

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,23 @@
-console.log('ok');
+const brickBlue = {
+    color: 'blue',
+    addNewBrick() {
+        console.log(this.color);
+    }
+};
+const newBrick = {
+    color: "red",
+    addNewBrick: () => {
+        console.log(newBrick, "newBrick");
+    }
+};
+function addNewBrick() {
+    const brickDiv = document.getElementById("brick");
+    const newSquare = document.createElement("div");
+    newSquare.style.backgroundColor = "red";
+    newSquare.style.width = "50px";
+    newSquare.style.height = "50px";
+    newSquare.style.margin = "5px";
+    brickDiv.appendChild(newSquare);
+}
+const addNewBrickButton = document.getElementById("addNewBrickButton");
+addNewBrickButton.addEventListener("click", addNewBrick);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,32 @@
-console.log('ok');
+interface Brick {
+    color: string;
+    addNewBrick: () => void;
+  }
+  
+  const brickBlue: Brick = {
+    color: 'blue',
+    addNewBrick() {
+      console.log(this.color);
+    }
+  };
+
+  const newBrick: Brick = {
+    color: "red",
+    addNewBrick: () => {
+      console.log(newBrick, "newBrick")
+    }
+  };
+  
+  function addNewBrick() {
+    const brickDiv = document.getElementById("brick");
+    const newSquare = document.createElement("div");
+    newSquare.style.backgroundColor = "red";
+    newSquare.style.width = "50px";
+    newSquare.style.height = "50px";
+    newSquare.style.margin = "5px";
+    brickDiv.appendChild(newSquare);
+  }
+
+  
+  const addNewBrickButton = document.getElementById("addNewBrickButton");
+  addNewBrickButton.addEventListener("click", addNewBrick);
