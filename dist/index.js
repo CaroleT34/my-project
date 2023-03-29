749ace8b9835f04a5410be69c88abcb99f803936
Carole TOULORGE <carole.toulorge@hotmail.fr>
2023-03-29 10:02:19 +0200

    modif index

--- dist/index.js
+++ dist/index.js
@@ -1,23 +1,3 @@
-const brickBlue = {
-    color: 'blue',
-    addNewBrick() {
-        console.log(this.color);
-    }
-};
-const newBrick = {
-    color: "red",
-    addNewBrick: () => {
-        console.log(newBrick, "newBrick");
-    }
-};
-function addNewBrick() {
-    const brickDiv = document.getElementById("brick");
-    const newSquare = document.createElement("div");
-    newSquare.style.backgroundColor = "red";
-    newSquare.style.width = "50px";
-    newSquare.style.height = "50px";
-    newSquare.style.margin = "5px";
-    brickDiv.appendChild(newSquare);
-}
-const addNewBrickButton = document.getElementById("addNewBrickButton");
-addNewBrickButton.addEventListener("click", addNewBrick);
+import { App } from './app/app';
+import './index.scss';
+App.run();
