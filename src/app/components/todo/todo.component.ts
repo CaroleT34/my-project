8458759f9619d553a418576ed827ddc8e1c6d2af
Carole TOULORGE <carole.toulorge@hotmail.fr>
2023-03-31 12:53:04 +0200

    Filtre par utilisateur relié aux taches + taches dans deux tableaux distincts

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
-import { MainComponent } from '../main/main.component';
+import { filter, map, Observable, of } from 'rxjs';
+import { Todo } from 'src/app/shared/models/todo';
+import { TodoService } from 'src/app/shared/services/todo.service';
 
 @Component({
   selector: 'app-todo',
@@ -7,6 +9,58 @@ import { MainComponent } from '../main/main.component';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent {
-  @Input() title: string;
+  @Input() id_user: string;
+
+  todos$ = this._todoService.todos$;
+  selected!: Todo;
+  todos : Todo[] = [];
+
+  constructor(
+    private _todoService: TodoService
+  ) {}
+
+  //Filter by user and done = false
+  getTodosByUserAndTaskToDo(id_user: string): Observable<Todo[]> {
+    if (id_user === "") {
+      return of([]); // Using of() from rxjs to create an observable that emits an empty array
+    } else {
+      const userFilterById: Todo[] = [];
+    this.todos$.value.forEach(todo => {
+      if (todo.id_user == id_user && todo.done == false) {
+        userFilterById.push(todo);
+      }
+    });
+      return of(userFilterById);
+    }
+  }
+
+   //Filter by user and done = true
+   getTodosByUserAndTaskDone(id_user: string): Observable<Todo[]> {
+    if (id_user === "") {
+      return of([]); // Using of() from rxjs to create an observable that emits an empty array
+    } else {
+      const userFilterById: Todo[] = [];
+    this.todos$.value.forEach(todo => {
+      if (todo.id_user == id_user && todo.done == true) {
+        userFilterById.push(todo);
+      }
+    });
+      return of(userFilterById);
+    }
+  }
+  
+
+  selectTodo(selected: Todo) {
+    this.selected = selected;
+  }
+
+  toggleStateTodo(todo: Todo) {
+    todo.done = !todo.done;
+    this._todoService.update(todo);
+  }
+
+  deleteTodo(id?: string) {
+    this._todoService.delete(id);
+  }
  
 }
--- src/app/shared/services/todo.service.ts
+++ src/app/shared/services/todo.service.ts
@@ -1,9 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, filter } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Todo } from '../models/todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  private apiUrl = environment.urlApi + '/todos';
+  public todos$ = new BehaviorSubject<Todo[]>([]);
 
-  constructor() { }
+  
+  constructor(private http: HttpClient) {
+    this.findAll();
+  }
+
+  public findAll() {
+    this.http
+      .get<Todo[]>(this.apiUrl)
+      .subscribe(todosFromApi => {
+        this.todos$.next(todosFromApi);
+      });
+  }
+
+  public update(todo: Todo) {
+    this.http
+      .put<Todo>(`${this.apiUrl}/${todo.id}`, todo)
+      .subscribe(() => this.findAll());
+  }
+
+  public delete(id?: string) {
+    if (id) {
+      this.http
+        .delete<Todo>(`${this.apiUrl}/${id}`)
+        .subscribe(() => this.findAll());
+    }
+  }
 }
